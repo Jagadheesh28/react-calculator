ea0695fbe70d9df668e1c9f38ae436b65e83e3b0
Jaga N <jagdheesh.n@makoitlab.com>
2024-01-17 13:53:46 +0530

    Added functions to the calculator UI and textcolor for Result

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
 "use client";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./globals.css";
 
 export default function Home() {
+  const [result, setResult] = useState("");
+  const [expression, setExpression] = useState("");
+
+  const handleButtonClick = (value: any) => {
+    if (value === "=") {
+      try {
+        setResult(eval(expression).toString());
+      } catch (error) {
+        setResult("Error");
+      }
+    } else if (value === "AC") {
+      setResult("");
+      setExpression("");
+    } else {
+      setExpression((prevExpression) => prevExpression + value);
+    }
+  };
+
   const buttons = [
     "7",
     "8",
@@ -29,13 +48,21 @@ export default function Home() {
       <div className=".bg-white p-6">
         <input
           type="text"
-          className="w-100 mb-2 fs-4 border-bottom border-2 border-gray-400"
+          className="w-100 mb-2 fs-4 border-bottom border-2 border-gray-400 text-dark"
+          value={expression}
+          readOnly
         />
-        <input type="text" className="w-100 fs-3 fw-bold mb-4" />
+        <input
+          type="text"
+          className="w-100 fs-3 fw-bold mb-4 text-danger"
+          value={result}
+          readOnly
+        />
         <div className="grid grid-cols-4 gap-2">
           {buttons.map((btn) => (
             <button
               key={btn}
+              onClick={() => handleButtonClick(btn)}
               className="btn btn-outline-secondary btn-lg rounded-lg bg-danger"
             >
               {btn}
